1f316b7193269a01719f5ba960cc5701307eb05f
seolhwayam <susung462@gmail.com>
2025-07-16 14:25:44 +0900

    commit

--- src/pages/Sample.jsx
+++ src/pages/Sample.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Greeting from '../components/Sample/Greeting'
 
 
@@ -10,11 +10,23 @@ const handleButtonClick = (data) =>{
     console.log("click",data);
 }
 
+const [userNames, setUserNames] = useState({});
+
+useEffect(() => {
+    //컴포넌트 실행 시 딱 한번만 실행
+    const nameDatas = ["코코볼","첵스초코","콘프로스트"]
+    setUserNames(nameDatas);
+
+},[]) //useEffect(() => {},[]) 기본 구문
+
+//view
   return (
     <div>sample
+        {userNames.map((userName)=> <Greeting key={`userName`} name={userName} onButtonClick={handleButtonClick}/>)}
+
         <Greeting name="김설화" onButtonClick={handleButtonClick}/>
         <Greeting name="코코볼" onButtonClick={handleButtonClick}/>
-        <Greeting onButtonClick={handleButtonClick} />
+        <Greeting name="코코" onButtonClick={handleButtonClick} />
 
     </div>
   )
