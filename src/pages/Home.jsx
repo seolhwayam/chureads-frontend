478850739647b6b2643c1879954665d7c119a280
seolhwayam <susung462@gmail.com>
2025-07-17 17:03:32 +0900

    succes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,36 +2,29 @@ import React, { useEffect, useState } from "react";
 import Header from "../components/layout/Header";
 import Nav from "../components/layout/Nav";
 import FeedItem from "../components/FeedItem";
-import { initialFeedList, initialTags } from "../data/response";
 import { useNavigate } from "react-router-dom";
-import { auth } from "../firebase";
+import { auth } from '../firebase';
 
 const Home = () => {
   // logic
   const history = useNavigate();
+  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
 
   const currentUser = auth.currentUser;
-  console.log("🚀 ~ Home ~ currentUser:", currentUser)
+  const isLoggedIn = !!currentUser
 
-  const isLoggedIn = !!currentUser;
-  
-
-  const [feedList, setFeedList] = useState(initialFeedList);
+  const [feedList, setFeedList] = useState([]);
 
   const handleEdit = (data) => {
     history(`/edit/${data._id}`); // edit페이지로 이동
   };
 
   const handleDelete = (selectedItem) => {
-    const filterList = feedList.filter((item) => item.id !== selectedItem.id);
+    const filterList = feedList.filter((item) => item._id !== selectedItem._id);
     setFeedList(filterList);
 
     // TODO: 백엔드에 Delete 요청
   };
-
-
-  // 미션 이미 로그인된 사용자는 login페이지 접근 불가
-  // home페이지로 리다이렉트 시키기
 
   const handleLike = (selectedId) => {
     console.log("🚀 ~ handleLike ~ selectedId:", selectedId)
@@ -39,57 +32,69 @@ const Home = () => {
 
   const handleLogout = async () => {
     // 1. firebase 로그아웃
-   
-    if(isLoggedIn){
+    if (isLoggedIn) {
       // 로그인된 상태인 경우
-      const ok = window.confirm("Are you sure Logout?");
-      ok && await auth.signOut();
+      const ok = window.confirm("Are you sure logout?")
+      ok && await auth.signOut()
     }
-    
     // 2. /login 으로 이동
-    history('/login');
-
-    // 3. 
+    history('/login')
   }
-
 
   useEffect(() => {
     // 페이지 진입시 딱 한번 실행
     // TODO: 백엔드에 Get 요청
-  }, []);
 
-   useEffect(()=>{
-    // 로그인되지 않은 사용자는 /login페이지로 이동
-    !isLoggedIn && history('/login');
-  
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(`${API_BASE_URL}/posts`)
+        if (!response.ok) {
+          throw new Error(`HTTP error: status: ${response.status}`)
+        }
 
+        const result = await response.json()
+        setFeedList(result)
+        console.log("🚀 ~ fetchPosts ~ result:", result)
+      } catch (error) {
+        console.error("게시물 조회 실패:", error)
+      }
+    }
+
+    fetchPosts()
+  }, [API_BASE_URL]);
+
+  useEffect(() => {
+    // 로그인되지 않은 사용자는 /login 페이지로 이동
+    !isLoggedIn && history('/login')
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   // view
   return (
     <div className="h-full pt-20 pb-[74px] overflow-hidden">
       {/* START: 헤더 영역 */}
-      <Header isLoggedIn={true} onClick = {handleLogout} />
+      <Header isLoggedIn={true} onClick={handleLogout} />
       {/* END: 헤더 영역 */}
       <main className="h-full overflow-auto">
         {/* TODO */}
 
         <div>
           {/* START: 피드 영역 */}
-          <ul>
+          {feedList.length ? <ul>
             {feedList.map((feed) => (
               <FeedItem
                 key={feed._id}
                 data={feed}
-                tags={initialTags}
-                isAuthor={true}
+                tags={feed.tags}
+                isAuthor={feed.userId === currentUser.uid}
                 onDelete={handleDelete}
                 onEdit={handleEdit}
                 onLike={handleLike}
               />
             ))}
-          </ul>
+          </ul> : <p>No Data</p>}
+
+
           {/* END: 피드 영역 */}
         </div>
       </main>
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,18 +1,44 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import PostInput from "../components/PostInput";
+import { auth } from "../firebase";
 
-//작성페이지
 const Post = () => {
   // logic
   const history = useNavigate();
+
+  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
+  const currentUser = auth.currentUser;
+
   const [churead, setChuread] = useState("");
 
   const handleChange = (value) => {
     setChuread(value);
   };
 
-  const handlePost = (event) => {
+  const createPost = async (postData) => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/posts`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postData)
+      })
+
+      if (!response.ok) {
+        throw new Error(`HTTP error: status: ${response.status}`)
+      }
+
+      const result = await response.json()
+      return result
+
+    } catch (error) {
+      console.error("게시글 추가 에러:", error)
+    }
+  }
+
+  const handlePost = async (event) => {
     event.preventDefault(); // 폼 제출시 새로고침 방지 메소드
 
     // 1. 텍스트에서 불필요한 공백 제거하기
@@ -29,6 +55,25 @@ const Post = () => {
 
     // 빈 스트링이 아닌 경우
     // TODO: 백엔드에 Post 요청
+    try {
+      const newItem = {
+        userName: currentUser.displayName,
+        userId: currentUser.uid,
+        userProfileImage: currentUser.photoURL || "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y",
+        content: resultChuread
+      }
+
+      // API 요청
+      const result = await createPost(newItem)
+      console.log("🚀 ~ result:", result)
+
+
+    } catch (error) {
+      console.error("게시글 추가 에러:", error)
+    }
+
+
+
 
     history("/"); // home화면으로 이동
   };
@@ -49,7 +94,7 @@ const Post = () => {
         <div className="h-full overflow-auto">
           <form id="post" onSubmit={handlePost}>
             {/* START: 사용자 입력 영역 */}
-            <PostInput onChange={handleChange} />
+            <PostInput userName={currentUser.displayName} userProfileImage={currentUser.photoURL || "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"} onChange={handleChange} />
             {/* END: 사용자 입력 영역 */}
             {/* START: 게시 버튼 영역 */}
             <div className="w-full max-w-[572px] flex items-center fixed bottom-0 lef p-6">
